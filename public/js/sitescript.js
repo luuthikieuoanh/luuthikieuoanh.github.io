153d3e25e24b004e0183c54ae71417878e8d56d3
luuthikieuoanh <67403607+luuthikieuoanh@users.noreply.github.com>
2020-07-16 02:31:58 +0700

    Gần xong header :heart:

--- public/js/sitescript.js
+++ public/js/sitescript.js
@@ -1,6 +1,7 @@
 //Hiện menu nav
 const a = document.querySelector('.navbar-toggler');
 const b = document.querySelector('#collapsibleNavId');
+//
 a.addEventListener("click", function() {
     b.classList.toggle('show');
 });
@@ -8,19 +9,37 @@ a.addEventListener("click", function() {
 //Bắt sự kiện lăn chuột
 const navBar = document.querySelector('.navbar');
 const logo = document.querySelector('a.logo');
-const discover = document.querySelector('discover');
+const c = document.querySelector('.header-inner');
+//
 window.addEventListener('scroll', () => {
     // console.log(window.pageYOffset)
-    if (window.pageYOffset > 5) {
+    if (window.pageYOffset > 1) {
         navBar.classList.add('fixed');
+        c.style.backgroundColor = 'rgba(0, 0, 0, 0.90)';
+        c.style.paddingTop = `${0}`;
         logo.classList.add('setlogo');
-        navBar.classList.add('setcontainer');
-        discover.classList.add('setdiscover');
     } else {
         navBar.classList.remove('fixed');
         logo.classList.remove('setlogo');
-        navBar.classList.remove('setcontainer');
-        discover.classList.remove('setdiscover');
-
+        c.style.backgroundColor = 'transparent';
+        c.style.paddingTop = `${20}px`;
     }
 })
+
+//Thay đổi độ dài của ảnh với các screen khác nhau
+const ax = document.querySelector('.slide-img-bg');
+
+function changeScreen() {
+    ax.style.width = `${screen.width}px`
+    ax.style.height = `${screen.height}px`
+}
+setInterval(changeScreen, 100);
+
+//Bắt sự kiện di chuyển chuột mousemove
+function myFunction(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    let rs = x + "," + y;
+    console.log(rs);
+
+}
