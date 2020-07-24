e51dc8687af395dc3d77bafd8741f4c0a6a9f731
luuthikieuoanh <67403607+luuthikieuoanh@users.noreply.github.com>
2020-07-25 06:47:58 +0700

    .......

--- public/js/sitescript.js
+++ public/js/sitescript.js
@@ -167,3 +167,24 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+//Carousel
+// $(document).ready(function() {
+//     $('.owl-carousel').owlCarousel({
+//         autoplay: true,
+//         autoplayTimeout: 5000,
+//         loop: true,
+//         margin: 10,
+//         nav: true,
+//         responsive: {
+//             0: {
+//                 items: 1
+//             },
+//             600: {
+//                 items: 1
+//             },
+//             1000: {
+//                 items: 1
+//             }
+//         }
+//     });
+// })
